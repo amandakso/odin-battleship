0a7ca63fc1d7d3d2d00b336459f3cb2d5573a6f6
Amanda So <soamanda95@gmail.com>
2022-08-05 23:47:20 -0700

    fix bug to remove excess event listeners from buttons

--- src/game.js
+++ src/game.js
@@ -7,30 +7,60 @@ const startGame = (player1, player2) => {
   renderButtons(player1, player2);
 };
 
-const resetGame = () => {
-  console.log('reset TBD');
-};
-
 const setUpGame = () => {
   const player1 = playerFactory('Player 1');
   const player2 = playerFactory('Computer'); // cpu
   player2.changeTurn();
   const getPlayer1 = () => player1;
   const getPlayer2 = () => player2;
-  const name = document.getElementById('name');
-  name.addEventListener('click', () => {
-    const info = document.querySelector('.player1Info');
+  // name event listener doesn't reset with reset button
+  const info = document.querySelector('.player1Info');
+  const para = document.createElement('p');
+  para.innerText = "Player Name: ";
+  info.appendChild(para);
+  const name = document.createElement('input');
+  name.type = 'text';
+  name.classList.add('name');
+  info.appendChild(name);
+  const nameBtn = document.createElement('button');
+  nameBtn.innerText = 'Submit';
+  info.appendChild(nameBtn);
+
+  nameBtn.addEventListener('click', () => {
     info.style.display = 'none';
-    player1.changeName(document.getElementById('player1').value);
-    document.getElementById('player1').value = '';
+    player1.changeName(document.querySelector('.name').value);
+    while (info.firstChild) {
+      info.removeChild(info.firstChild);
+    }
     setUpBoard(player1);
   });
-  const start = document.getElementById('start');
+  const startBtn = document.querySelector('.startBtn');
+  const start = document.createElement('button');
+  start.innerText = 'Start Game';
+  startBtn.appendChild(start);
   start.addEventListener('click', () => {
     startGame(player1, player2);
   });
-  const reset = document.getElementById('reset');
+  const resetBtn = document.querySelector('.resetBtn');
+  const reset = document.createElement('button');
+  reset.innerText = 'Reset Game';
+  resetBtn.appendChild(reset);
   reset.addEventListener('click', () => {
+    const resetGame = () => {
+      while (startBtn.firstChild) {
+        startBtn.removeChild(startBtn.firstChild);
+      }
+      while (resetBtn.firstChild) {
+        resetBtn.removeChild(resetBtn.firstChild);
+      }
+      deleteBoards();
+      const setup = setUpGame();
+      const p1 = setup.getPlayer1();
+      const p2 = setup.getPlayer2();
+      const p1Gameboard = p1.getGameboard();
+      const p2Gameboard = p2.getGameboard();
+      p2Gameboard.generateRandomFleet();
+    };
     resetGame();
   });
   return {
